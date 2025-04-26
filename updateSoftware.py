8d5673b44e190f8477096b3dcb88ae456eb61582
Kanerock17yt <kanerock17@gmail.com>
2025-04-26 09:51:09 -0600

    Add files via upload

--- updateSoftware.py
+++ updateSoftware.py
@@ -2,6 +2,14 @@ import os
 import time
 import random
 
+# Functions
+def start_text():
+    print("Thanks for choosing updateSoftware!")
+    print("Now it is going to start updating.")
+    print("Please wait. Geting files...")
+    time.sleep(random.randint(1, 2))
+    input("Press Enter to start updating.")
+
 def update_software():
     print("Updating.")
     time.sleep(1)
@@ -11,6 +19,7 @@ def update_software():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Updating...")
     time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Updating.")
     time.sleep(1)
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -19,13 +28,9 @@ def update_software():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Updating...")
 
-def start_text():
-    print("Thanks for choosing updateSoftware!")
-    print("Now it is going to start updating.")
-    print("Please wait. Geting files...")
-    time.sleep(random.randint(1, 2))
-    input("Press Enter to start updating.")
 
+
+# Code
 start_text()
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -36,3 +41,5 @@ time.sleep(random.randint(1, 3))
 os.system('cls' if os.name == 'nt' else 'clear')
 
 print("Updated software successfully!")
+
+input("Press Enter to exit.")
