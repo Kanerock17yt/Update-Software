0264c45d89193e9a24b1349133a250de65415968
Kanerock17yt <kanerock17@gmail.com>
2025-04-27 07:18:46 -0600

    Add files via upload

--- updateSoftware.py
+++ updateSoftware.py
@@ -6,9 +6,40 @@ import random
 def start_text():
     print("Thanks for choosing updateSoftware!")
     print("Now it is going to start updating.")
-    print("Please wait. Geting files...")
+    
     time.sleep(random.randint(1, 2))
     input("Press Enter to start updating.")
+    time.sleep(random.randint(1, 3))
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    def print_getingFiles():
+        print("Please wait. Geting files.")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please wait. Geting files..")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please wait. Geting files...")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please wait. Geting files.")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please wait. Geting files..")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please wait. Geting files...")
+
+        time.sleep(1)        
+        print("Got files.")
+        time.sleep(2)
+    
+    print_getingFiles()
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    
+
 
 def update_software():
     print("Updating.")
@@ -27,6 +58,12 @@ def update_software():
     time.sleep(1)
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Updating...")
+
+    
+    print("Preparing to finish update.")
+    print("May take 5 seconds.")
+    time.sleep(random.randint(1, 5))
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 
